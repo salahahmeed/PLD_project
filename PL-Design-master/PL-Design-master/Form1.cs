9876dd9cf4f90f6f9bbcdd791ced4c3ebae71265
salahahmeed <119974288+salahahmeed@users.noreply.github.com>
2023-05-09 16:45:40 +0300

    Add files via upload

---
+++ PL-Design-master/PL-Design-master/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using com.calitha.commons;
+using com.calitha.goldparser;
+
+namespace PLD
+{
+    public partial class Form1 : Form
+    {
+        MyParser parser;
+        DataTable dt = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+            parser = new MyParser("Mygrammer.cgt", listBox1, dt);
+            dgv.DataSource = dt;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            parser.Parse(richTextBox1.Text);
+            dgv.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
